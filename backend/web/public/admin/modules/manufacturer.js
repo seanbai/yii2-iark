e005faf52848b3d700228ee26cad879121f1de84
sean.bai <sean.bai@silksoftware.com>
2020-05-08 22:44:07 +0800

    字段放出来,供下载

--- backend/web/public/admin/modules/manufacturer.js
+++ backend/web/public/admin/modules/manufacturer.js
@@ -23,12 +23,18 @@ layui.define(function(exports){
       cols: [[ //表头
         {field: 'username', title: '用户名',templet:'<div>{{d.username}}</div>'},
         {field: 'brand', title: '供应品牌',templet:'<div>{{d.name}}</div>'},
+        {field: 'main', title: '签约主体',templet:'<div>{{d.main}}</div>'},
+        {field: 'url', title: '官方网站',templet:'<div>{{d.url}}</div>'},
         {field: 'boss', title: '订单员',templet:'<div>{{d.contact}}</div>'},
         {field: 'phone', title: '联系电话',templet:'<div>{{d.phone}}</div>'},
-        {field: 'mail', title: '邮件',templet:'<div>{{d.email}}</div>'},
+        {field: 'mail', title: '邮箱',templet:'<div>{{d.email}}</div>'},
+        {field: 'boss2', title: '备用联系人',templet:'<div>{{d.contact2}}</div>'},
+        {field: 'phone2', title: '备用联系电话',templet:'<div>{{d.phone2}}</div>'},
+        {field: 'mail2', title: '备用邮箱',templet:'<div>{{d.email2}}</div>'},
         {field: 'status', title: '账户状态', templet:'#userStatus'},
         {field: 'city', title: '城市',templet:'<div>{{d.city}}</div>'},
         {field: 'address', title: '提货地址',templet:'<div>{{d.address}}</div>'},
+        {field: 'off', title: '供货折扣',templet:'<div>{{d.off}}</div>'},
         {field: 'contract', title: '签约时间',templet:'<div>{{d.time}}</div>'},
         {fixed: 'right', title:'操作', toolbar: '#editTool', width:140}
       ]]
